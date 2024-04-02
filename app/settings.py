2bf0e584ccc47e8a67930a74ef3921892ae87bee
alex <a.3aremba@gmail.com>
2024-04-03 00:23:28 +0200

    feat: change format for allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SITE_ID=1
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,0.0.0.0,*.ondigitalocean.app").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,0.0.0.0,.ondigitalocean.app").split(",")
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
