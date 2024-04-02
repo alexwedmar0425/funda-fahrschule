8a906b6c7563bfaca8307ba03c6f824a8c60783b
alex <a.3aremba@gmail.com>
2024-04-03 00:11:54 +0200

    fix: wsgi path

--- app/wsgi.py
+++ app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funda-fahrschule.app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
